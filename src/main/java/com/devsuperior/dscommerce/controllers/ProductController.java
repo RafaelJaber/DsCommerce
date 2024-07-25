b02c42be24ca5410ee1f5dd7d0156e86097be6b5
Rafael Jáber <rafael.jaber@hotmail.com>
2024-07-25 14:16:04 -0300

    ✨ (ProductController.java, ProductService.java): Adiciona suporte à paginação na listagem de produtos Adiciona suporte à paginação na listagem de produtos, permitindo que os resultados sejam retornados de forma paginada, o que melhora a performance e a experiência do usuário ao lidar com grandes conjuntos de dados. A adição das classes Page e Pageable do Spring Framework permite a implementação da paginação de forma eficiente e organizada.

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -2,12 +2,16 @@ package com.devsuperior.dscommerce.controllers;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -24,5 +28,12 @@ public class ProductController {
         ProductDTO dto = productService.findById(productId);
 
         return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> list = productService.findAll(pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 }
--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -3,9 +3,12 @@ package com.devsuperior.dscommerce.services;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,5 +27,12 @@ public class ProductService {
         );
 
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = productRepository.findAll(pageable);
+
+        return result.map(ProductDTO::new);
     }
 }
