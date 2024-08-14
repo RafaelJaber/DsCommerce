972e8ed5def206bad5a5e72f868e7d5a84fd8e08
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-13 21:25:24 -0300

    🔒 (ProductController.java): Adiciona autorização de acesso para funções de administrador Adiciona a anotação @PreAuthorize("hasAnyRole('ROLE_ADMIN')") em métodos de inserção, atualização e exclusão de produtos no controlador ProductController. Isso garante que apenas usuários com a função de administrador tenham permissão para executar essas operações, aumentando a segurança e controle de acesso no sistema.

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -38,6 +39,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productDTOPage);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
         ProductDTO inserted = productService.insert(dto);
@@ -46,6 +48,7 @@ public class ProductController {
         return ResponseEntity.created(uri).body(inserted);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
         ProductDTO updated = productService.update(id, dto);
@@ -53,6 +56,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         productService.deleteById(id);
