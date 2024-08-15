55595e524d178f43ff8407aef5b8e29f1bdd5df9
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-14 22:17:03 -0300

    ✨ (OrderItemDTO.java): Adiciona imgUrl ao DTO de OrderItem Adiciona a propriedade imgUrl ao DTO de OrderItem para representar a URL da imagem do produto associado ao item do pedido. Isso enriquece as informações disponíveis no DTO e pode ser útil para exibir imagens relacionadas aos itens do pedido em interfaces de usuário.

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -16,12 +16,14 @@ public class OrderItemDTO {
     private String name;
     private Double price;
     private Integer quantity;
+    private String imgUrl;
 
     public OrderItemDTO(OrderItem orderItem) {
         this.productId = orderItem.getProduct().getId();
         this.name = orderItem.getProduct().getName();
         this.price = orderItem.getPrice();
         this.quantity = orderItem.getQuantity();
+        this.imgUrl = orderItem.getProduct().getImgUrl();
     }
 
     public Double getSubTotal() {
