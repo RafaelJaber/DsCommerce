ef298debf387f628ed3f54abe81981b9cb70f44b
Rafael Jaber <rafael.jaber@hotmail.com>
2024-08-20 16:27:12 -0300

    ✨ Adiciona testes unitários para o serviço de pedidos (OrderService) 📝 Adiciona classe OrderFactory para criação de objetos de pedido (Order) nos testes unitários

---
+++ src/test/java/com/devsuperior/dscommerce/services/OrderServiceTests.java
@@ -0,0 +1,88 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.repositories.OrderItemRepository;
+import com.devsuperior.dscommerce.repositories.OrderRepository;
+import com.devsuperior.dscommerce.services.exceptions.ForbiddenException;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscommerce.tests.OrderFactory;
+import com.devsuperior.dscommerce.tests.UserFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@Tag("Unit")
+@ExtendWith(SpringExtension.class)
+public class OrderServiceTests {
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private AuthService authService;
+
+    private Long existingOrderId, nonExistingOrderId;
+    private Order order;
+    private OrderDTO orderDTO;
+    private User admin, client;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingOrderId = 1L;
+        nonExistingOrderId = 2L;
+        admin = UserFactory.createCustomAdminUser(1L, "Jef");
+        client = UserFactory.createCustomClientUser(2L, "John");
+        order = OrderFactory.createOrder(client);
+        orderDTO = new OrderDTO(order);
+
+        Mockito.when(orderRepository.findById(existingOrderId)).thenReturn(Optional.of(order));
+        Mockito.when(orderRepository.findById(nonExistingOrderId)).thenReturn(Optional.empty());
+    }
+
+    @Test
+    public void findByIdShouldReturnOrderDTOWhenIdExistsAndAdminLogged() {
+        Mockito.doNothing().when(authService).validateSelfOrAdmin(any());
+
+        OrderDTO result = orderService.findById(existingOrderId);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getId(), existingOrderId);
+
+        Mockito.verify(authService).validateSelfOrAdmin(any());
+        Mockito.verify(orderRepository, Mockito.times(1)).findById(existingOrderId);
+    }
+
+    @Test
+    public void findByIdShouldThrowsResourceNotFoundExceptionWhenIdDoesNotExistAndAdminLogged() {
+        Mockito.doNothing().when(authService).validateSelfOrAdmin(any());
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            orderService.findById(nonExistingOrderId);
+        });
+
+        Mockito.verify(orderRepository, Mockito.times(1)).findById(nonExistingOrderId);
+    }
+
+    @Test
+    public void findByIdShouldThrowsForbiddenExceptionWhenIdExistsAndOtherClientLogged() {
+        Mockito.doThrow(ForbiddenException.class).when(authService).validateSelfOrAdmin(any());
+
+        Assertions.assertThrows(ForbiddenException.class, () -> {
+            orderService.findById(existingOrderId);
+        });
+    }
+}
